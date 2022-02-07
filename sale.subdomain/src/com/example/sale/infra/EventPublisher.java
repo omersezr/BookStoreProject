607ae3b58654cf82553757a207df92264be46bb4
omersezr <56982247+omersezr@users.noreply.github.com>
2022-02-07 17:18:45 +0300

    Delete sale.subdomain directory

--- sale.subdomain/src/com/example/sale/infra/EventPublisher.java
+++
@@ -1,7 +0,0 @@
-package com.example.sale.infra;
-
-import com.example.sale.application.business.event.SaleEvent;
-
-public interface EventPublisher<Event> {
-	void publishEvent(SaleEvent businessevent);	
-}
