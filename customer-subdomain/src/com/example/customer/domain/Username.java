950470525bad56e75bc91b1f0ab96354c893eba0
Naciye Aydin <naciyeaydin994@gmail.com>
2022-02-08 10:59:55 +0300

    Update Username.java

--- customer-subdomain/src/com/example/customer/domain/Username.java
+++ customer-subdomain/src/com/example/customer/domain/Username.java
@@ -1,5 +1,55 @@
 package com.example.customer.domain;
 
+import java.util.Objects;
+
 public class Username {
+	
+	private String userName;
+
+	public Username(String userName) {
+		this.userName = userName;
+	}
+	
+	public static Username of(String userName) {
+		
+		Objects.requireNonNull(userName);
+		
+		
+		if (userName.length() < 5)
+			throw new IllegalArgumentException("Kullanıcı adınız 5 karakterden az olamaz. ");
+		
+		return new Username(userName);
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Username other = (Username) obj;
+		return Objects.equals(userName, other.userName);
+	}
+
+	@Override
+	public String toString() {
+		return "Username [userName=" + userName + "]";
+	}
+	
 
 }
