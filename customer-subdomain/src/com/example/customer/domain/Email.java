ef03263fc0eeedd28d0676a886181f4f1fe366b6
Naciye Aydin <naciyeaydin994@gmail.com>
2022-02-08 10:46:20 +0300

    Update Email.java

--- customer-subdomain/src/com/example/customer/domain/Email.java
+++ customer-subdomain/src/com/example/customer/domain/Email.java
@@ -1,5 +1,31 @@
 package com.example.customer.domain;
 
 public class Email {
+	
+	private String email;
+	
+	public Email(String email) {
+		super();
+		this.email = email;
+	}
+	
+	
+	public static Email of(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        
+        // Email kontrolü false ise 
+        if (m.matches() == false)
+        	throw new IllegalArgumentException("Lütfen doğru bir email adresi giriniz.");
+        		
+        return new Email(email);
+ }
+ 
+	public String getEmail() {
+		return email;
+	}
+	
+	
 
 }
